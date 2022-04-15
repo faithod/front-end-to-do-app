d9ec22960d4b1342f90f7ebdeb495d23eab6b352
Faith <faithodularu@hotmail.com>
2022-04-15 15:26:52 +0100

    changed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header(): JSX.Element {
   return (
     <header>
-      <h1>What do you need to do today?</h1>
+      <h1>What do you need to do this week?</h1>
     </header>
   );
 }
